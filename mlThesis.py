c808f49d77caa2e1d09b06cca8ab50d0a8e87d06
M A Olivari <m.a.olivari@student.vu.nl>
2017-04-21 19:23:26 +0200

    Formatting Dataset: WIP (Work in Progress) code upload test

---
+++ mlThesis.py
@@ -0,0 +1,308 @@
+import numpy as np  # linear algebra library
+import xgboost as xgb  # ensemble boosted tree model (don't have to import this yet!)
+import pandas as pd  # data processing library, converts data into data frames and allows for manipulation of these frames
+import sklearn as sk
+from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import KFold
+import time
+import datetime
+import matplotlib as plt
+
+#TARGET = "Verschil_bedtijd_dag1"
+#NFOLDS = 5
+#SEED = 0
+#NROWS = None
+
+pd.set_option('expand_frame_repr', False)  # shows dataframe without wrapping to next line
+pd.set_option('chained_assignment', None)
+pd.set_option('display.max_columns', 500)
+
+data_path = "C:/Users/Michael/Desktop/BachelorThesis/"  # my working directory
+data_file = data_path + "LISS_bedtime.csv"
+
+df = pd.read_csv(data_file)
+#print(df)
+
+#----- Drop Unusable Data (already been transformed in another manner) -----#
+df = df.drop("Reason4_nl", 1)
+df = df.drop("Reason_overig", 1)
+df = df.drop("brutoink", 1)
+df = df.drop("brutoink_f", 1)
+df = df.drop("nettoink", 1)
+df = df.drop("nettoink_f", 1)
+df = df.drop("netinc", 1)
+df = df.drop("brutohh_f", 1)
+df = df.drop("nettohh_f", 1)
+
+#----- Drop Survey Specific Responses (Not Predictors) -----#
+df = df.drop("im13a_m", 1)
+df = df.drop("Vragenmoeilijk", 1)
+df = df.drop("Vragenduidelijk", 1)
+df = df.drop("Eval1", 1)
+df = df.drop("Eval2", 1)
+df = df.drop("Eval3", 1)
+df = df.drop("opm", 1)
+df = df.drop("evaopm", 1)
+df = df.drop("Baseline_datumstart", 1)
+df = df.drop("Baseline_tijdstart", 1)
+df = df.drop("Baseline_datumeinde", 1)
+df = df.drop("Baseline_tijdeinde", 1)
+df = df.drop("Baseline_duur", 1)
+df = df.drop("Dagboek_jaarmaand", 1)
+df = df.drop("in13a031", 1)
+df = df.drop("in13a032", 1)
+df = df.drop("in13a033", 1)
+df = df.drop("in13a034", 1)
+df = df.drop("in13a035", 1)
+df = df.drop("in13a057", 1)
+df = df.drop("in13a058", 1)
+df = df.drop("in13a059", 1)
+df = df.drop("in13a060", 1)
+df = df.drop("in13a061", 1)
+df = df.drop("in13a083", 1)
+df = df.drop("in13a084", 1)
+df = df.drop("in13a085", 1)
+df = df.drop("in13a086", 1)
+df = df.drop("in13a087", 1)
+df = df.drop("in13a109", 1)
+df = df.drop("in13a110", 1)
+df = df.drop("in13a111", 1)
+df = df.drop("in13a112", 1)
+df = df.drop("in13a113", 1)
+df = df.drop("in13a135", 1)
+df = df.drop("in13a136", 1)
+df = df.drop("in13a137", 1)
+df = df.drop("in13a138", 1)
+df = df.drop("in13a139", 1)
+df = df.drop("in13a161", 1)
+df = df.drop("in13a162", 1)
+df = df.drop("in13a163", 1)
+df = df.drop("in13a164", 1)
+df = df.drop("in13a165", 1)
+df = df.drop("in13a187", 1)
+df = df.drop("in13a188", 1)
+df = df.drop("in13a189", 1)
+df = df.drop("in13a190", 1)
+df = df.drop("in13a191", 1)
+df = df.drop("filter_$", 1)
+df = df.drop("slaap6_r", 1)
+
+#---------Drop Day Remarks and Unusable External Factor Specifics -----#
+df = df.drop("Extern_nl_dag1", 1)
+df = df.drop("Andersnl_dag1", 1)
+df = df.drop("opm_dag1", 1)
+df = df.drop("evaopm_dag1", 1)
+df = df.drop("Extern_nl_dag2", 1)
+df = df.drop("Andersnl_dag2", 1)
+df = df.drop("opm_dag2", 1)
+df = df.drop("evaopm_dag2", 1)
+df = df.drop("Extern_nl_dag3", 1)
+df = df.drop("Andersnl_dag3", 1)
+df = df.drop("opm_dag3", 1)
+df = df.drop("evaopm_dag3", 1)
+df = df.drop("Extern_nl_dag4", 1)
+df = df.drop("Andersnl_dag4", 1)
+df = df.drop("opm_dag4", 1)
+df = df.drop("evaopm_dag4", 1)
+df = df.drop("Extern_nl_dag5", 1)
+df = df.drop("Andersnl_dag5", 1)
+df = df.drop("opm_dag5", 1)
+df = df.drop("evaopm_dag5", 1)
+df = df.drop("Extern_nl_dag6", 1)
+df = df.drop("Andersnl_dag6", 1)
+df = df.drop("opm_dag6", 1)
+df = df.drop("evaopm_dag6", 1)
+df = df.drop("Extern_nl_dag7", 1)
+df = df.drop("Andersnl_dag7", 1)
+df = df.drop("opm_dag7", 1)
+df = df.drop("evaopm_dag7", 1)
+
+#--------------- Drop Partner Data (Not Enough) and Bedtime Data per day (Not to be used as predictors) ------------#
+df = df.drop("GeplandeBedtijd_dag1", 1)
+df = df.drop("EchteBedtijd_dag1", 1)
+df = df.drop("Slaaptijd_dag1", 1)
+df = df.drop("Opstaan_dag1", 1)
+df = df.drop("Bedtijd_partner_dag1", 1)
+df = df.drop("Slaaptijd_partner_dag1", 1)
+df = df.drop("Opstaan_partner", 1)
+df = df.drop("GeplandeBedtijd_dag2", 1)
+df = df.drop("EchteBedtijd_dag2", 1)
+df = df.drop("Slaaptijd_dag2", 1)
+df = df.drop("Opstaan_dag2", 1)
+df = df.drop("Bedtijd_partner_dag2", 1)
+df = df.drop("Slaaptijd_partner_dag2", 1)
+df = df.drop("Opstaan_partner_dag2", 1)
+df = df.drop("GeplandeBedtijd_dag3", 1)
+df = df.drop("EchteBedtijd_dag3", 1)
+df = df.drop("Slaaptijd_dag3", 1)
+df = df.drop("Opstaan_dag3", 1)
+df = df.drop("Bedtijd_partner_dag3", 1)
+df = df.drop("Slaaptijd_partner_dag3", 1)
+df = df.drop("Opstaan_partner_dag3", 1)
+df = df.drop("GeplandeBedtijd_dag4", 1)
+df = df.drop("EchteBedtijd_dag4", 1)
+df = df.drop("Slaaptijd_dag4", 1)
+df = df.drop("Opstaan_dag4", 1)
+df = df.drop("Bedtijd_partner_dag4", 1)
+df = df.drop("Slaaptijd_partner_dag4", 1)
+df = df.drop("Opstaan_partner_dag4", 1)
+df = df.drop("GeplandeBedtijd_dag5", 1)
+df = df.drop("EchteBedtijd_dag5", 1)
+df = df.drop("Slaaptijd_dag5", 1)
+df = df.drop("Opstaan_dag5", 1)
+df = df.drop("Bedtijd_partner_dag5", 1)
+df = df.drop("Slaaptijd_partner_dag5", 1)
+df = df.drop("Opstaan_partner_dag5", 1)
+df = df.drop("GeplandeBedtijd_dag6", 1)
+df = df.drop("EchteBedtijd_dag6", 1)
+df = df.drop("Slaaptijd_dag6", 1)
+df = df.drop("Opstaan_dag6", 1)
+df = df.drop("Bedtijd_partner_dag6", 1)
+df = df.drop("Slaaptijd_partner_dag6", 1)
+df = df.drop("Opstaan_partner_dag6", 1)
+df = df.drop("GeplandeBedtijd_dag7", 1)
+df = df.drop("EchteBedtijd_dag7", 1)
+df = df.drop("Slaaptijd_dag7", 1)
+df = df.drop("Opstaan_dag7", 1)
+df = df.drop("Bedtijd_partner_dag7", 1)
+df = df.drop("Slaaptijd_partner_dag7", 1)
+df = df.drop("Opstaan_partner_dag7", 1)
+df = df.drop("in13a026", 1)
+df = df.drop("in13a028", 1)
+df = df.drop("in13a030", 1)
+df = df.drop("in13a052", 1)
+df = df.drop("in13a054", 1)
+df = df.drop("in13a056", 1)
+df = df.drop("in13a078", 1)
+df = df.drop("in13a080", 1)
+df = df.drop("in13a082", 1)
+df = df.drop("in13a104", 1)
+df = df.drop("in13a106", 1)
+df = df.drop("in13a108", 1)
+df = df.drop("in13a130", 1)
+df = df.drop("in13a132", 1)
+df = df.drop("in13a134", 1)
+df = df.drop("in13a156", 1)
+df = df.drop("in13a158", 1)
+df = df.drop("in13a160", 1)
+df = df.drop("in13a182", 1)
+df = df.drop("in13a184", 1)
+df = df.drop("in13a186", 1)
+df = df.drop("Verschil_bedtijd_week", 1)
+df = df.drop("Verschil_bedtijd_5dagen", 1)
+df = df.drop("Verschil_bedtijd_weekend", 1)
+
+binaryAttributesToEncode = [
+    "Nachtdienst",
+    "Reason_nvt",
+    "Extern_dag1_geldig",
+    "Extern_dag2_geldig",
+    "Extern_dag3_geldig",
+    "Extern_dag4_geldig",
+    "Extern_dag5_geldig",
+    "Extern_dag6_geldig",
+    "Extern_dag7_geldig",
+    ]
+
+timeAttributesToEncode = [
+    'Verschil_bedtijd_dag1',
+    'Verschil_bedtijd_dag2',
+    'Verschil_bedtijd_dag3',
+    'Verschil_bedtijd_dag4',
+    'Verschil_bedtijd_dag5',
+    'Verschil_bedtijd_dag6',
+    'Verschil_bedtijd_dag7']
+
+def applyBinary(attribute):
+    try:
+        return int(attribute)
+    except:
+        return 2
+
+def timeToMinutes(hhmmss):
+    if hhmmss.strip():
+        [hours, minutes, seconds] = [int(x) for x in hhmmss.split(':')]
+        t = datetime.timedelta(hours=hours, minutes=minutes, seconds=seconds)
+        return t.total_seconds() / 60
+    else:
+        return np.NaN
+
+for attribute in binaryAttributesToEncode:
+    df[attribute] = df[attribute].apply(lambda x: applyBinary(x))
+
+for attribute in timeAttributesToEncode:
+    df[attribute] = df[attribute].apply(lambda x: timeToMinutes(x))
+
+df = df.replace(r'\s+', np.nan, regex=True)
+print(df)
+df = df.dropna()
+print(df.shape)
+
+
+
+
+#-----------------------------Split Dataframe into Train and Test Sets------------------------------#
+
+#train, test = train_test_split(df, train_size = 0.7)
+
+#ntrain = train.shape[0]
+#ntest = test.shape[0]
+
+#x_train = np.array(df[:train.shape[0]])
+#x_test = np.array(df[train.shape[0]:])
+
+#y_train = np.log(train[TARGET]+1)
+#train.drop([TARGET], axis=1, inplace=True)
+#kf = KFold(ntrain, n_folds=NFOLDS, shuffle=True, random_state=SEED)
+
+
+
+#------ Model Training -------#
+class XgbWrapper(object):
+    def __init__(self, seed=0, params=None):
+        self.param = params
+        self.param['seed'] = seed
+        self.nrounds = params.pop('nrounds', 250)
+
+    def train(self, x_train, y_train):
+        dtrain = xgb.DMatrix(x_train, label=y_train)
+        self.gbdt = xgb.train(self.param, dtrain, self.nrounds)
+
+    def predict(self, x):
+        return self.gbdt.predict(xgb.DMatrix(x))
+
+xgb_params = {
+    'seed': 0,
+    'colsample_bytree': 0.7,
+    'silent': 1,
+    'subsample': 0.7,
+    'learning_rate': 0.075,
+    'objective': 'reg:linear',
+    'max_depth': 4,
+    'num_parallel_tree': 1,
+    'min_child_weight': 1,
+    'eval_metric': 'rmse',
+    'nrounds': 500
+}
+
+def get_oof(model): # get best set up out of folds of cross validation
+    oof_train = np.zeros((ntrain,))
+    oof_test = np.zeros((ntest,))
+    oof_test_skf = np.empty((NFOLDS, ntest))
+
+    for i, (train_index, test_index) in enumerate(kf):
+        x_tr = x_train[train_index]
+        y_tr = y_train[train_index]
+        x_te = x_train[test_index]
+
+        model.train(x_tr, y_tr)
+
+        oof_train[test_index] = model.predict(x_te)
+        oof_test_skf[i, :] = model.predict(x_test)
+
+    oof_test[:] = oof_test_skf.mean(axis=0)
+    return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+#xg = XgbWrapper(seed=SEED, params=xgb_params)
+#xg_oof_train, xg_oof_test = get_oof(xg)
+
